0873cea33d6cf417f1cf6899f6b95f4745c4dee2
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2023-01-14 13:24:34 +0700

    Add solution for the first problem day 10

---
+++ Day10-1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <deque>
+#include <map>
+#include <algorithm>
+#include <string>
+#include<sstream>
+#include <cstring>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <iomanip>
+#include <set>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pii;
+vector<string> split(string s)
+{
+    vector<string> a;
+    stringstream ss(s);
+    string word;
+    while (ss>>word) {
+        a.push_back(word);
+    }
+    return a;
+}
+vector<string> split(string s, char del)
+{
+    vector<string> a;
+    stringstream ss(s);
+    string word;
+    while (!ss.eof()) {
+        getline(ss, word,del);
+        a.push_back(word);
+    }
+    return a;
+}
+ll toInt(string s){
+    ll res=0;
+    for(int i=0;i<s.size();i++){
+        res*=10;
+        res+=(s[i]-'0');
+    }
+    return res;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    string s;
+    vector<pii> order; //{val,completed?}
+    order.push_back({0,1});
+    while(cin>>s){
+        if(s=="noop"){
+            order.push_back({0,1});
+        }
+        else{
+            int val;
+            cin>>val;
+            order.push_back({val,0});
+            order.push_back({val,1});
+        }
+    }
+    int res=0,X=1;
+    for(int i=1;i<order.size();i++){
+        if((i-20)%40==0){
+            res+=X*i;
+        }
+        if(order[i].second==1){
+            X+=order[i].first;
+        }
+    }
+    cout<<res;
+    return 0;
+}
